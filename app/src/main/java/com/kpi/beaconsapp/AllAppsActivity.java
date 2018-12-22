e40826c0dcbf0ac27d799799487fb5f506af57a7
Alex314 <alex3.14mart@gmail.com>
2018-12-22 21:37:09 +0200

    Remove apps number restriction, add run on click

--- app/src/main/java/com/kpi/beaconsapp/AllAppsActivity.java
+++ app/src/main/java/com/kpi/beaconsapp/AllAppsActivity.java
@@ -2,53 +2,60 @@ package com.kpi.beaconsapp;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 
 public class AllAppsActivity extends AppCompatActivity {
     public ArrayList<String> apps;
+    public HashMap<String, String> app_code;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        apps = new ArrayList<>();
+        app_code = new HashMap<>();
         setContentView(R.layout.all_apps_activity);
 
         ListView allAppsView =  findViewById(R.id.all_apps_list) ;
         final PackageManager pm = getPackageManager(); //get a list of installed apps.
         List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
-        apps = new ArrayList<>();
-        int i = 20;
         for (ApplicationInfo pkg: packages) {
             if((pm.getLaunchIntentForPackage(pkg.packageName) != null) ){ // & (((pkg.flags & ApplicationInfo.FLAG_SYSTEM) == 0))) {
-                apps.add((String) pm.getApplicationLabel(pkg) + " " + pkg.packageName);
+                apps.add((String) pm.getApplicationLabel(pkg));
+                app_code.put((String) pm.getApplicationLabel(pkg), pkg.packageName);
             }
-            i = i-1;
-            if (i <= 0) break;
         }
+        apps.sort(String::compareTo);
+
 
         Log.i("AllAppsActivity","" +apps.size() + " apps added");
-//        Intent launch = getPackageManager().getLaunchIntentForPackage(code);
-//        startActivity(launch);
         AllAppsActivity.AppItemAdapter appItemAdapter = new AllAppsActivity.AppItemAdapter(this, apps);
         allAppsView.setAdapter(appItemAdapter);
-//        allBeaconsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                showNotesNotifications(db.getBeacons().get(i).getCode());
-//            }
-//        });
+
+        allAppsView.setOnItemClickListener((adapterView, view, i, l) -> {
+            String packageName = app_code.get(apps.get(i));
+            Intent launch = getPackageManager().getLaunchIntentForPackage(packageName);
+            startActivity(launch);
+        });
     }
 
     class AppItemAdapter extends BaseAdapter {
@@ -83,11 +90,6 @@ public class AllAppsActivity extends AppCompatActivity {
             view = layoutInflater.inflate(R.layout.apps_list_item_layout, null);
 
             TextView appNameView = view.findViewById(R.id.appNameView);
-//            Log.i("AllAppsActivity","" +appNameView.toString() + " appNameView");
-//            Log.i("AllAppsActivity","apps contain " +apps.size() + " elements");
-//            Log.i("AllAppsActivity","" +i + "th element requested");
-//            Log.i("AllAppsActivity","" +apps.get(i) + " element requested");
-
             appNameView.setText(apps.get(i));
             return view;
         }
